38ff1806600a2e569ea3923b47268a15f6605802
Dreyrk <lucas.rondepierre123@gmail.com>
2023-01-11 14:56:14 +0100

    go

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap";
-// import $ from "jquery"
-// import Popper from "popper.js"
 import "./styles/index.css";
 import App from "./App";
 
